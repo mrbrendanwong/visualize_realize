38beb9152f236dab19b975298511349e8f5ac186
mrbrendanwong <brendonydwong@gmail.com>
2018-11-14 23:12:46 -0800

    Add basic error handling

--- src/data-controller.js
+++ src/data-controller.js
@@ -4,45 +4,80 @@ module.exports = {getContributors, getContent, getBranches, getBranchCommits, ge
 
 // Get the contributors of a repository
 async function getContributors(owner, repo) {
-    const result = await octokit.repos.getContributors({owner: owner, repo: repo, anon: "true"});
-    console.log(result);
+    let result;
+    try {
+        result = await octokit.repos.getContributors({owner: owner, repo: repo, anon: "true"});
+        console.log(result);
+    } catch (e) {
+        console.error("HttpError", e);
+    }
 }
 
 // Get the contents of a GitHub repository. To get root path, just supply empty string
 // To get all files; call root, scan for all dirs, recursively call getContent
 async function getContent(owner, repo, path) {
-    const result = await octokit.repos.getContent({owner: owner, repo: repo, path: path});
-    console.log(result);
+    let result;
+    try {
+        result = await octokit.repos.getContent({owner: owner, repo: repo, path: path});
+        console.log(result);
+    } catch (e) {
+        console.error("HttpError", e);
+    }
 }
 
 // Get the branches of a repo
 async function getBranches(owner, repo) {
-    const result = await octokit.repos.getBranches({owner: owner, repo: repo});
-    console.log(result);
+    let result;
+    try {
+        result = await octokit.repos.getBranches({owner: owner, repo: repo});
+        console.log(result);
+    } catch (e) {
+        console.error("HttpError", e);
+    }
 }
 
 // Get the commits of a branch. If branch param is empty, will default to master
 async function getBranchCommits(owner, repo, branch) {
     if (branch === "") branch = "master";
-    const result = await octokit.repos.getCommits({owner: owner, repo: repo, sha: branch});
-    console.log(result);
+    let result;
+    try {
+        result = await octokit.repos.getCommits({owner: owner, repo: repo, sha: branch});
+        console.log(result);
+    } catch (e) {
+        console.error("HttpError", e);
+    }
 }
 
 // Get a single commit
 async function getCommit(owner, repo, commit_sha) {
-    const result = await octokit.repos.getCommit({owner: owner, repo: repo, sha: commit_sha});
-    console.log(result);
+    let result;
+    try {
+        result = await octokit.repos.getCommit({owner: owner, repo: repo, sha: commit_sha});
+        console.log(result);
+    } catch (e) {
+        console.error("HttpError", e);
+    }
 }
 
 // Get the commit comments of a commit
 async function getCommitComments(owner, repo, commit_sha) {
-    const result = await octokit.repos.getCommitComments({owner: owner, repo: repo, ref: commit_sha});
-    console.log(result);
+    let result;
+    try {
+        result = await octokit.repos.getCommitComments({owner: owner, repo: repo, ref: commit_sha});
+        console.log(result);
+    } catch (e) {
+        console.error("HttpError", e);
+    }
 }
 
 // Get a the blob of a file. Need to process decode blob from Base64
 async function getBlob(owner, repo, file_sha) {
-    const result = await octokit.gitdata.getBlob({owner, repo, file_sha});
-    console.log(result);
+    let result;
+    try {
+        result = await octokit.gitdata.getBlob({owner, repo, file_sha});
+        console.log(result);
+    } catch (e) {
+        console.error("HttpError", e);
+    }
 }
 
