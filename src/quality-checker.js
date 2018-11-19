03de55301d29a534c41a5bb75c3f6fb0962480e0
Thomas Zhou <thethomaszhou@gmail.com>
2018-11-18 21:57:45 -0800

    Write results to csv file

--- src/quality-checker.js
+++ src/quality-checker.js
@@ -1,14 +1,19 @@
 const { spawn } = require('child_process');
+const fs = require('fs');
 
 module.exports = {checkCode};
 
 function checkCode() {
+    let resultStream = fs.createWriteStream('pmd_results.csv');
+
     const pmd = spawn('resources/pmd/bin/run.sh',
-        ['pmd', '-d', 'resources/TestFile.java', '-R', 'resources/rulesets/quickstart.xml',
+        ['pmd', '-d', '../beta_engine/src', '-R', 'resources/rulesets/quickstart.xml',
             '-f', 'csv', '-failOnViolation', 'false']);
 
     pmd.stdout.on('data', (data) => {
+        resultStream.write(data);
         console.log(`stdout: ${data}`);
+
     });
 
     pmd.stderr.on('data', (data) => {
