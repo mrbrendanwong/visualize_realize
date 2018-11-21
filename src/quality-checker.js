5e8c393abaec285f65d2f08d509f2986cae62f01
Thomas Zhou <thethomaszhou@gmail.com>
2018-11-20 23:27:30 -0800

    Write results to json file

--- src/quality-checker.js
+++ src/quality-checker.js
@@ -1,4 +1,5 @@
 const { spawn } = require('child_process');
+const fs = require('fs');
 const Papa = require('papaparse');
 
 module.exports = {checkCode};
@@ -9,9 +10,9 @@ const papaparseConfig = {
 };
 let problemsFound = [];
 
-function checkCode() {
+function checkCode(sourceDirectory, outputFileLocation) {
     const pmd = spawn('resources/pmd/bin/run.sh',
-        ['pmd', '-d', '../beta_engine/src', '-R', 'resources/rulesets/quickstart.xml',
+        ['pmd', '-d', sourceDirectory, '-R', 'resources/rulesets/quickstart.xml',
             '-f', 'csv', '-failOnViolation', 'false']);
 
     let csvStream = pmd.stdout.pipe(Papa.parse(Papa.NODE_STREAM_INPUT, papaparseConfig));
@@ -25,6 +26,8 @@ function checkCode() {
 
     pmd.on('close', (code) => {
         console.log(`pmd child process exited with code ${code}`);
-        console.log(problemsFound);
+        fs.writeFile(outputFileLocation, JSON.stringify(problemsFound), (error) => {
+            error ? console.log(error) : null;
+        });
     });
 }
